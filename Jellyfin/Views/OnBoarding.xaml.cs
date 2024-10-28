6e35d40a2d6fdb776468b48b0c5e21742c8452a8
David Federman <david.federman@outlook.com>
2024-10-27 22:47:01 -0700

    Add Jellyfin.Sdk and kick the tires on the Onboarding view

--- Jellyfin/Views/OnBoarding.xaml.cs
+++ Jellyfin/Views/OnBoarding.xaml.cs
@@ -1,22 +1,8 @@
-﻿using Jellyfin.Core;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Threading.Tasks;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿using System;
+using Jellyfin.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace Jellyfin.Views
 {
@@ -44,30 +30,41 @@ namespace Jellyfin.Views
         private async void BtnConnect_Click(object sender, RoutedEventArgs e)
         {
             btnConnect.IsEnabled = false;
-            txtError.Visibility = Visibility.Collapsed;
-
-            string uriString = txtUrl.Text;
             try
             {
-                var ub = new UriBuilder(uriString);
-                uriString = ub.ToString();
+                txtError.Visibility = Visibility.Collapsed;
+
+                string serverUrl = txtUrl.Text;
+                if (!Uri.IsWellFormedUriString(serverUrl, UriKind.Absolute))
+                {
+                    UpdateErrorMessage($"Invalid url: {serverUrl}");
+                    return;
+                }
+
+                JellyfinService.Instance.SetServerUrl(serverUrl);
+                try
+                {
+                    // Get public system info to verify that the url points to a Jellyfin server.
+                    var systemInfo = await JellyfinService.Instance.Client.System.Info.Public.GetAsync();
+                    Console.WriteLine($"Connected to {serverUrl}");
+                    Console.WriteLine($"Server Name: {systemInfo.ServerName}");
+                    Console.WriteLine($"Server Version: {systemInfo.Version}");
+                }
+                catch (Exception ex)
+                {
+                    UpdateErrorMessage($"Error connecting to {serverUrl}: {ex.Message}");
+                    return;
+                }
+
+                // Save the Url in settings
+                Central.Settings.JellyfinServer = serverUrl;
+
+                Frame.Navigate(typeof(MainPage));
             }
             finally
             {
-                ; //If the UriBuilder fails the following functions will handle the error
+                btnConnect.IsEnabled = true;
             }
-
-            if (!await CheckURLValidAsync(uriString))
-            {
-                txtError.Visibility = Visibility.Visible;
-            }
-            else
-            {
-                Central.Settings.JellyfinServer = uriString;
-                (Window.Current.Content as Frame).Navigate(typeof(MainPage));
-            }
-
-            btnConnect.IsEnabled = true;
         }
 
         private void OnBoarding_Loaded(object sender, RoutedEventArgs e)
@@ -75,80 +72,10 @@ namespace Jellyfin.Views
             txtUrl.Focus(FocusState.Programmatic);
         }
 
-        private async Task<bool> CheckURLValidAsync(string uriString)
-        {
-            // also do a check for valid url
-            if (!Uri.IsWellFormedUriString(uriString, UriKind.Absolute))
-            {
-                return false;
-            }
-
-            //add scheme to uri if not included 
-            Uri testUri = new UriBuilder(uriString).Uri;
-
-            // check URL exists
-            HttpWebRequest request;
-            HttpWebResponse response;
-            try
-            {
-                request = (HttpWebRequest)WebRequest.Create(testUri);
-                response = (HttpWebResponse)(await request.GetResponseAsync());
-            }
-            catch (WebException ex)
-            {
-                // Handle web exceptions here
-                if (ex.Response != null && ex.Response is HttpWebResponse errorResponse)
-                {
-                    int statusCode = (int)errorResponse.StatusCode;
-                    if (statusCode >= 300 && statusCode <= 308)
-                    {
-                        // Handle Redirect
-                        string newLocation = errorResponse.Headers["Location"];
-                        if (!string.IsNullOrEmpty(newLocation))
-                        {
-                            uriString = newLocation;
-                            return await CheckURLValidAsync(uriString); // Recursively check the new location
-                        }
-                    }
-                    else
-                    {
-                        UpdateErrorMessage(statusCode);
-                    }
-                    return false;
-                }
-                else
-                {
-                    // Handle other exceptions
-                    return false;
-                }
-            }
-
-            if (response == null || response.StatusCode != HttpStatusCode.OK)
-            {
-                return false;
-            }
-
-            var encoding = System.Text.Encoding.GetEncoding(response.CharacterSet);
-            using (var reader = new System.IO.StreamReader(response.GetResponseStream(), encoding))
-            {
-                string responseText = reader.ReadToEnd();
-                if (!responseText.Contains("Jellyfin"))
-                {
-                    return false;
-                }
-            }
-
-            // If everything is OK, update the URI before saving it
-            Central.Settings.JellyfinServer = uriString;
-
-            return true;
-        }
-
-
-        private void UpdateErrorMessage(int statusCode)
+        private void UpdateErrorMessage(string message)
         {
             txtError.Visibility = Visibility.Visible;
-            txtError.Text = $"Error: {statusCode}";
+            txtError.Text = message;
         }
     }
 }

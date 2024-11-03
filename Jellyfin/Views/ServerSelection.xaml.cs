1afbfb497bce07a3cbc368441cd3cd7517a69c6a
David Federman <dfederm@microsoft.com>
2024-11-03 15:55:18 -0800

    Migrate ServerSelection page to MVVM

--- Jellyfin/Views/ServerSelection.xaml.cs
+++ Jellyfin/Views/ServerSelection.xaml.cs
@@ -1,98 +1,23 @@
-﻿using System;
-using Jellyfin.Core;
+﻿using Jellyfin.Core;
 using Jellyfin.Sdk;
 using Microsoft.Extensions.DependencyInjection;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Input;
 
 namespace Jellyfin.Views;
 
 public sealed partial class ServerSelection : Page
 {
-    private readonly AppSettings _appSettings;
-    private readonly JellyfinSdkSettings _sdkClientSettings;
-    private readonly JellyfinApiClient _jellyfinApiClient;
-
     public ServerSelection()
     {
-        // TODO: Is there a better way to do DI in UWP?
-        _appSettings = AppServices.Instance.ServiceProvider.GetRequiredService<AppSettings>();
-        _sdkClientSettings = AppServices.Instance.ServiceProvider.GetRequiredService<JellyfinSdkSettings>();
-        _jellyfinApiClient = AppServices.Instance.ServiceProvider.GetRequiredService<JellyfinApiClient>();
-
         InitializeComponent();
-        Loaded += ServerSelection_Loaded;
 
-        if (_appSettings.ServerUrl is not null)
-        {
-            ServerUrlText.Text = _appSettings.ServerUrl;
-        }
+        // TODO: Is there a better way to do DI in UWP?
+        AppSettings appSettings = AppServices.Instance.ServiceProvider.GetRequiredService<AppSettings>();
+        JellyfinSdkSettings sdkClientSettings = AppServices.Instance.ServiceProvider.GetRequiredService<JellyfinSdkSettings>();
+        JellyfinApiClient jellyfinApiClient = AppServices.Instance.ServiceProvider.GetRequiredService<JellyfinApiClient>();
+
+        ViewModel = new ServerSelectionViewModel(appSettings, sdkClientSettings, jellyfinApiClient);
     }
 
-    private void ServerUrlText_KeyUp(object sender, KeyRoutedEventArgs e)
-    {
-        if (e.Key == Windows.System.VirtualKey.Enter)
-        {
-            ConnectButton_Click(ConnectButton, null);
-        }
-    }
-
-    private async void ConnectButton_Click(object sender, RoutedEventArgs e)
-    {
-        ConnectButton.IsEnabled = false;
-        try
-        {
-            ErrorText.Visibility = Visibility.Collapsed;
-
-            string serverUrl = ServerUrlText.Text;
-
-            // Add protocol if needed
-            if (!serverUrl.Contains("://", StringComparison.Ordinal))
-            {
-                serverUrl = "https://" + serverUrl;
-            }
-
-            if (!Uri.IsWellFormedUriString(serverUrl, UriKind.Absolute))
-            {
-                UpdateErrorMessage($"Invalid url: {serverUrl}");
-                return;
-            }
-
-            _sdkClientSettings.SetServerUrl(serverUrl);
-            try
-            {
-                // Get public system info to verify that the url points to a Jellyfin server.
-                var systemInfo = await _jellyfinApiClient.System.Info.Public.GetAsync();
-                Console.WriteLine($"Connected to {serverUrl}");
-                Console.WriteLine($"Server Name: {systemInfo.ServerName}");
-                Console.WriteLine($"Server Version: {systemInfo.Version}");
-            }
-            catch (Exception ex)
-            {
-                UpdateErrorMessage($"Error connecting to {serverUrl}: {ex.Message}");
-                return;
-            }
-
-            // Save the Url in settings
-            _appSettings.ServerUrl = serverUrl;
-
-            Frame.Navigate(typeof(Login));
-        }
-        finally
-        {
-            ConnectButton.IsEnabled = true;
-        }
-    }
-
-    private void ServerSelection_Loaded(object sender, RoutedEventArgs e)
-    {
-        ServerUrlText.Focus(FocusState.Programmatic);
-    }
-
-    private void UpdateErrorMessage(string message)
-    {
-        ErrorText.Visibility = Visibility.Visible;
-        ErrorText.Text = message;
-    }
+    public ServerSelectionViewModel ViewModel { get; }
 }
---
+++ Jellyfin/Views/ServerSelectionViewModel.cs
@@ -0,0 +1,88 @@
+using System;
+using Jellyfin.Core;
+using Jellyfin.Sdk;
+
+namespace Jellyfin.Views;
+
+public sealed class ServerSelectionViewModel : BindableBase
+{
+    private readonly AppSettings _appSettings;
+    private readonly JellyfinSdkSettings _sdkClientSettings;
+    private readonly JellyfinApiClient _jellyfinApiClient;
+
+    public ServerSelectionViewModel(AppSettings appSettings, JellyfinSdkSettings sdkClientSettings, JellyfinApiClient jellyfinApiClient)
+    {
+        _appSettings = appSettings;
+        _sdkClientSettings = sdkClientSettings;
+        _jellyfinApiClient = jellyfinApiClient;
+
+        if (_appSettings.ServerUrl is not null)
+        {
+            ServerUrl = _appSettings.ServerUrl;
+        }
+
+        IsInteractable = true;
+    }
+
+    public bool IsInteractable { get; set => SetProperty(ref field, value); }
+
+    public string ErrorMessage { get; set => SetProperty(ref field, value); }
+
+    public bool ShowErrorMessage { get; set => SetProperty(ref field, value); }
+
+    public string ServerUrl { get; set => SetProperty(ref field, value); }
+
+    public async void Connect()
+    {
+        IsInteractable = false;
+        try
+        {
+            ShowErrorMessage = false;
+
+            string serverUrl = ServerUrl;
+
+            // Add protocol if needed
+            if (!serverUrl.Contains("://", StringComparison.Ordinal))
+            {
+                serverUrl = "https://" + serverUrl;
+            }
+
+            if (!Uri.IsWellFormedUriString(serverUrl, UriKind.Absolute))
+            {
+                UpdateErrorMessage($"Invalid url: {serverUrl}");
+                return;
+            }
+
+            _sdkClientSettings.SetServerUrl(serverUrl);
+            try
+            {
+                // Get public system info to verify that the url points to a Jellyfin server.
+                var systemInfo = await _jellyfinApiClient.System.Info.Public.GetAsync();
+                Console.WriteLine($"Connected to {serverUrl}");
+                Console.WriteLine($"Server Name: {systemInfo.ServerName}");
+                Console.WriteLine($"Server Version: {systemInfo.Version}");
+            }
+            catch (Exception ex)
+            {
+                UpdateErrorMessage("We're unable to connect to the selected server right now. Please ensure it is running and try again.");
+                Console.Error.WriteLine($"Error connecting to {serverUrl}: {ex.Message}");
+                return;
+            }
+
+            // Save the Url in settings
+            _appSettings.ServerUrl = serverUrl;
+
+            App.AppFrame.Navigate(typeof(Login));
+        }
+        finally
+        {
+            IsInteractable = true;
+        }
+    }
+
+    private void UpdateErrorMessage(string message)
+    {
+        ShowErrorMessage = true;
+        ErrorMessage = message;
+    }
+}

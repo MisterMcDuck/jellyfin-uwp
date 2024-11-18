eb1faea9a31344274a6251f8c1e2b5e800e08f5b
Scrooge McDuck <MMD-github@chrispr.org>
2024-11-17 20:16:55 -0800

    Initial commit of media play session tracking and progress updating

--- Jellyfin/Views/Video.xaml.cs
+++ Jellyfin/Views/Video.xaml.cs
@@ -15,13 +15,16 @@ public sealed partial class Video : Page
         // TODO: Is there a better way to do DI in UWP?
         JellyfinApiClient jellyfinApiClient = AppServices.Instance.ServiceProvider.GetRequiredService<JellyfinApiClient>();
         JellyfinSdkSettings sdkClientSettings = AppServices.Instance.ServiceProvider.GetRequiredService<JellyfinSdkSettings>();
+        AppSettings appSettings = AppServices.Instance.ServiceProvider.GetRequiredService<AppSettings>();
 
-        ViewModel = new VideoViewModel(jellyfinApiClient, sdkClientSettings, PlayerElement);
+        ViewModel = new VideoViewModel(jellyfinApiClient, sdkClientSettings, PlayerElement, appSettings);
     }
 
     internal VideoViewModel ViewModel { get; }
 
     protected override void OnNavigatedTo(NavigationEventArgs e) => ViewModel.HandleParameters(e.Parameter as Parameters);
+
+    protected override void OnNavigatingFrom(NavigatingCancelEventArgs e) => ViewModel.LeavingPlayer();
 
     public record Parameters(Guid VideoId, int? VideoStreamIndex, int? AudioStreamIndex, int? SubtitleStreamIndex);
 }
--- Jellyfin/Views/VideoViewModel.cs
+++ Jellyfin/Views/VideoViewModel.cs
@@ -1,12 +1,16 @@
 using System;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using Jellyfin.Common;
 using Jellyfin.Sdk;
+using Jellyfin.Sdk.Generated.Models;
 using Microsoft.Kiota.Abstractions;
 using Windows.Media.Core;
 using Windows.Media.Playback;
 using Windows.Media.Streaming.Adaptive;
 using Windows.Security.ExchangeActiveSyncProvisioning;
+using Windows.UI.Core;
 using Windows.UI.Xaml.Controls;
 
 namespace Jellyfin.Views;
@@ -16,19 +20,25 @@ public sealed class VideoViewModel : BindableBase
     private readonly JellyfinApiClient _jellyfinApiClient;
     private readonly JellyfinSdkSettings _sdkClientSettings;
     private readonly MediaPlayerElement _playerElement;
+    private readonly AppSettings _appSettings;
+    private Guid _playingVideoId;
+    private string _playingSessionId;
+    private Timer _progressTimer;
 
-    public VideoViewModel(JellyfinApiClient jellyfinApiClient, JellyfinSdkSettings sdkClientSettings, MediaPlayerElement playerElement)
+    public VideoViewModel(JellyfinApiClient jellyfinApiClient, JellyfinSdkSettings sdkClientSettings, MediaPlayerElement playerElement, AppSettings appSettings)
     {
         _jellyfinApiClient = jellyfinApiClient;
         _sdkClientSettings = sdkClientSettings;
         _playerElement = playerElement;
+        _appSettings = appSettings;
     }
 
     public async void HandleParameters(Video.Parameters parameters)
     {
-        Guid videoId = parameters.VideoId;
+        Guid videoId = _playingVideoId = parameters.VideoId;
 
-        // TODO: Create play session and set PlaySessionId
+        PlaybackInfoResponse playbackInfo = await _jellyfinApiClient.Items[videoId].PlaybackInfo.GetAsync();
+
         RequestInformation videoStreamRequest = _jellyfinApiClient.Videos[videoId].MainM3u8.ToGetRequestInformation(request =>
         {
             request.QueryParameters.MediaSourceId = videoId.ToString("N");
@@ -50,6 +60,7 @@ public sealed class VideoViewModel : BindableBase
             request.QueryParameters.SegmentContainer = "mp4";
             request.QueryParameters.MinSegments = 1;
             request.QueryParameters.BreakOnNonKeyFrames = true;
+            request.QueryParameters.PlaySessionId = playbackInfo.PlaySessionId;
             //request.QueryParameters.Level = "3";
             //request.QueryParameters.VideoBitRate = 8;
             //request.QueryParameters.Profile = "advanced";
@@ -70,12 +81,134 @@ public sealed class VideoViewModel : BindableBase
             _playerElement.SetMediaPlayer(new MediaPlayer());
             _playerElement.MediaPlayer.Source = MediaSource.CreateFromAdaptiveMediaSource(ams);
             _playerElement.MediaPlayer.Play();
+            
 
             ams.InitialBitrate = ams.AvailableBitrates.Max();
+
+            _playerElement.MediaPlayer.MediaEnded += (mp, o) =>
+            {
+                _ = _jellyfinApiClient.UserPlayedItems[videoId].PostAsync();
+            };
+            _playerElement.MediaPlayer.PlaybackSession.PlaybackStateChanged += async (session, obj) =>
+            {
+                switch (session.PlaybackState)
+                {
+                    case MediaPlaybackState.Paused:
+                        _ = _jellyfinApiClient.Sessions.Playing.Progress.PostAsync(new PlaybackProgressInfo()
+                        {
+                            ItemId = videoId,
+                            MediaSourceId = videoId.ToString("N"),
+                            AudioStreamIndex = parameters.AudioStreamIndex,
+                            SubtitleStreamIndex = parameters.SubtitleStreamIndex,
+                            PlaySessionId = _playingSessionId,
+                            PositionTicks = await GetCurrentTicks(),
+                            SessionId = _appSettings.SessionId,
+                            IsPaused = true
+                        });
+                        break;
+                    case MediaPlaybackState.Playing:
+                        _ = _jellyfinApiClient.Sessions.Playing.Progress.PostAsync(new PlaybackProgressInfo()
+                        {
+                            CanSeek = true,
+                            ItemId = videoId,
+                            MediaSourceId = videoId.ToString("N"),
+                            AudioStreamIndex = parameters.AudioStreamIndex,
+                            SubtitleStreamIndex = parameters.SubtitleStreamIndex,
+                            PlaySessionId = _playingSessionId,
+                            PositionTicks = await GetCurrentTicks(),
+                            SessionId = _appSettings.SessionId,
+                            IsPaused = false
+                        });
+                        break;
+                }
+            };
+
+            await _jellyfinApiClient.PlayingItems[videoId].PostAsync(request =>
+            {
+                request.QueryParameters.MediaSourceId = videoId.ToString("N");
+                request.QueryParameters.AudioStreamIndex = parameters.AudioStreamIndex;
+                request.QueryParameters.SubtitleStreamIndex = parameters.SubtitleStreamIndex;
+                request.QueryParameters.PlaySessionId = _playingSessionId = playbackInfo.PlaySessionId;
+                // TODO: do we need to support sessions/sessionId?
+                request.QueryParameters.CanSeek = _playerElement.MediaPlayer.PlaybackSession.CanSeek;
+
+            });
+
+            _progressTimer = new Timer(async (o) =>
+            {
+                if (await IsPlaying())
+                {
+                    Int64 currentTicks = await GetCurrentTicks();
+                    _ = _jellyfinApiClient.PlayingItems[videoId].Progress.PostAsync(request =>
+                    {
+                        request.QueryParameters.MediaSourceId = videoId.ToString("N");
+                        request.QueryParameters.AudioStreamIndex = parameters.AudioStreamIndex;
+                        request.QueryParameters.SubtitleStreamIndex = parameters.SubtitleStreamIndex;
+                        request.QueryParameters.PlaySessionId = _playingSessionId = playbackInfo.PlaySessionId;
+                        request.QueryParameters.PositionTicks = currentTicks;
+
+                    });
+                    _ = _jellyfinApiClient.Sessions.Playing.Progress.PostAsync(new PlaybackProgressInfo()
+                    {
+                        CanSeek = true,
+                        ItemId = videoId,
+                        MediaSourceId = videoId.ToString("N"),
+                        AudioStreamIndex = parameters.AudioStreamIndex,
+                        SubtitleStreamIndex = parameters.SubtitleStreamIndex,
+                        PlaySessionId = _playingSessionId,
+                        PositionTicks = await GetCurrentTicks(),
+                        SessionId = _appSettings.SessionId
+                    });
+                }
+            }, videoId, TimeSpan.FromMinutes(1), TimeSpan.FromMinutes(1));
         }
         else
         {
             // Handle failure to create the adaptive media source
         }
+    }
+
+    private async Task<Int64> GetCurrentTicks()
+    {
+        Int64 currentTicks = 0;
+        await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+                () => currentTicks = (int)_playerElement.MediaPlayer.PlaybackSession.Position.TotalMilliseconds * 10000);
+        return currentTicks > 0 ? currentTicks : 0;
+    }
+    private async Task<bool> IsPlaying()
+    {
+        bool playing = false;
+        await RunOnUIThread(() =>
+        {
+            playing = _playerElement.MediaPlayer.PlaybackSession.PlaybackState == MediaPlaybackState.Playing;
+        });
+        return playing;
+    }
+    private async Task RunOnUIThread(DispatchedHandler action)
+    {
+        await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, action);
+    }
+
+    public async void LeavingPlayer()
+    {
+        _progressTimer.Change(Timeout.Infinite, Timeout.Infinite);
+        _progressTimer = null;
+        Int64 currentTicks = await GetCurrentTicks();
+        await _jellyfinApiClient.Sessions.Playing.Stopped.PostAsync(new PlaybackStopInfo()
+        {
+            ItemId = _playingVideoId,
+            PlaySessionId = _playingSessionId,
+            PositionTicks = currentTicks,
+            SessionId = _appSettings.SessionId
+        });
+        /*
+        await _jellyfinApiClient.PlayingItems[_playingVideoId].DeleteAsync(request =>
+        {
+            request.QueryParameters.MediaSourceId = _playingVideoId.ToString("N");
+            request.QueryParameters.PlaySessionId = _playingSessionId;
+            request.QueryParameters.PositionTicks = currentTicks;
+
+        });
+        */
     }
 }
